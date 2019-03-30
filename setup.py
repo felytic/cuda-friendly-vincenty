26444aeefa6fc32bd1005f6528f5a8487a0d1941
felytic <felytic@gmail.com>
2019-03-30 15:10:22 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,14 +4,15 @@ except ImportError:
     from distutils.core import setup
 
 setup(
-    name='vincenty',
-    version='0.1.4',
-    description='Calculate the geographical distance between 2 points with extreme accuracy.',
+    name='cuda-friendly-vincenty',
+    version='0.1',
+    description='Function for geographical distances calculation '
+    'that can run on GPU using CUDA.',
     long_description=open('README.rst').read(),
-    author='Maurycy Pietrzak',
-    author_email='github.com@wayheavy.com',
-    url='https://github.com/maurycyp/vincenty',
-    packages=['vincenty'],
+    author='Serhii Hulko',
+    author_email='felytic@gmail.com',
+    url='https://github.com/felytic/cuda-friendly-vincenty',
+    packages=['cuda-friendly-vincenty'],
     license='Unlicense',
     classifiers=[
         'Development Status :: 4 - Beta',
@@ -23,5 +24,4 @@ setup(
         'Programming Language :: Python :: 3',
         'Topic :: Scientific/Engineering :: Mathematics',
     ],
-    # TODO zip_safe, package_data
 )
